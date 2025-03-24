fae21da41f00c147a22311721c551ca695013c47
ibz1330 <ibz0502291330@gmail.com>
2025-03-24 17:32:03 +0200

    Add card styling and functionality for country display

--- js/app.js
+++ js/app.js
@@ -1,21 +1,205 @@
-const getUrlByName = (_name)=>{
+import Country from './countryClass.js';
+const restartCards = ()=>{
+    const cards = document.querySelector(".cards");
+    cards.innerHTML = "";
+}
+const getUrlByName = (_name) => {
     return `https://restcountries.com/v3.1/name/${_name}`
 }
-const creatObj = (_item)=>{
+
+export const renderInStart = ()=>{
+    restartCards();
+    const countries = ["israel", "france", "united states", "thailand"];
+    countries.forEach(element => {
+        doApi(getUrlByName(element));
+    });
 }
-const doApi = (_url)=>{
+const creatObj = (_item) => {
+    const coin = Object.keys(_item.currencies);
+    const country = new Country(_item.name.common, _item.population, _item.region, _item.languages, coin, _item.capital[0], _item.flags)
+    country.render();
+}
+const renderBySearch = ()=>{
+    const search = document.querySelector("#search_id");
+    search.addEventListener('input', ()=>{
+        restartCards();
+        console.log(search.value);
+        doApi(getUrlByName(search.value))
+    })
+}
+const doApi = (_url) => {
     fetch(_url)
-    .then(response =>{
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}`);
+            }
+            return response.json()
+                .then(data => {
+                    // console.log(data[0].flags);
+                    
+                    creatObj(data[0])
+                    // console.log(`data ${JSON.stringify(data, null, 2)}`);
+                })
+                // .catch(err => {
+                //     console.log(`error: ${err}`);
+                // })
+        })
+}
+
+const listCountries = (_data)=>{
+    let arr = [];
+    _data.forEach(element => {
+        arr.push(element.name.common)
+    });
+    console.log(arr);
+    
+    return arr;
+}
+
+
+const getUrlAll = () => {
+    return `https://restcountries.com/v3.1/all`
+}
+let arrCountries = [];
+const doApi2 = (_url)=>{
+    fetch(_url).then(response =>{
         if(! response.ok){
-            throw new Error(`Error ${response.status}`);
+            throw Error (`error ${response.status}`)
         }
         return response.json()
         .then(data =>{
-            console.log(`data ${JSON.stringify(data, null, 2)}`);
+            console.log(data);
+           console.log( listCountries(data)[1]);
+           
+            
+            // console.log("data: "+JSON.stringify(data, null, 2));
         })
-        .catch(err =>{
-            console.log(`error: ${err}`);
-        })
+        // .catch(err =>{
+        //     console.log(`Error ${err}`);
+        // })
     })
 }
-doApi(getUrlByName("israel"));
+
+// doApi2(getUrlAll())
+renderInStart();
+renderBySearch();
+
+// [
+//     {
+//         "name": {
+//             "common": "Israel",
+//             "official": "State of Israel",
+//             "nativeName": {
+//                 "ara": {
+//                     "official": "دولة إسرائيل",
+//                     "common": "إسرائيل"
+//                 },
+//                 "heb": {
+//                     "official": "מדינת ישראל",
+//                     "common": "ישראל"
+//                 }
+//             }
+//         },
+//         "tld": [
+//             ".il"
+//         ],
+//         "cca2": "IL",
+//         "ccn3": "376",
+//         "cca3": "ISR",
+//         "cioc": "ISR",
+//         "independent": true,
+//         "status": "officially-assigned",
+//         "unMember": true,
+//         "currencies": {
+//             "ILS": {
+//                 "name": "Israeli new shekel",
+//                 "symbol": "₪"
+//             }
+//         },
+//         "idd": {
+//             "root": "+9",
+//             "suffixes": [
+//                 "72"
+//             ]
+//         },
+//         "capital": [
+//             "Jerusalem"
+//         ],
+//         "altSpellings": [
+//             "IL",
+//             "State of Israel",
+//             "Medīnat Yisrā'el"
+//         ],
+//         "region": "Asia",
+//         "subregion": "Western Asia",
+//         "languages": {
+//             "ara": "Arabic",
+//             "heb": "Hebrew"
+//         },
+//         "latlng": [
+//             31.47,
+//             35.13
+//         ],
+//         "landlocked": false,
+//         "borders": [
+//             "EGY",
+//             "JOR",
+//             "LBN",
+//             "PSE",
+//             "SYR"
+//         ],
+//         "area": 20770,
+//         "demonyms": {
+//             "eng": {
+//                 "f": "Israeli",
+//                 "m": "Israeli"
+//             },
+//             "fra": {
+//                 "f": "Israélienne",
+//                 "m": "Israélien"
+//             }
+//         },
+//         "flag": "🇮🇱",
+//         "maps": {
+//             "googleMaps": "https://goo.gl/maps/6UY1AH8XeafVwdC97",
+//             "openStreetMaps": "https://www.openstreetmap.org/relation/1473946"
+//         },
+//         "population": 9216900,
+//         "gini": {
+//             "2016": 39
+//         },
+//         "fifa": "ISR",
+//         "car": {
+//             "signs": [
+//                 "IL"
+//             ],
+//             "side": "right"
+//         },
+//         "timezones": [
+//             "UTC+02:00"
+//         ],
+//         "continents": [
+//             "Asia"
+//         ],
+//         "flags": {
+//             "png": "https://flagcdn.com/w320/il.png",
+//             "svg": "https://flagcdn.com/il.svg",
+//             "alt": "The flag of Israel has a white field with a blue hexagram — the Magen David — centered between two equal horizontal blue bands situated near the top and bottom edges of the field."
+//         },
+//         "coatOfArms": {
+//             "png": "https://mainfacts.com/media/images/coats_of_arms/il.png",
+//             "svg": "https://mainfacts.com/media/images/coats_of_arms/il.svg"
+//         },
+//         "startOfWeek": "sunday",
+//         "capitalInfo": {
+//             "latlng": [
+//                 31.77,
+//                 35.23
+//             ]
+//         },
+//         "postalCode": {
+//             "format": "#####",
+//             "regex": "^(\\d{5})$"
+//         }
+//     }
+// ]
--- js/countryClass.js
+++ js/countryClass.js
@@ -1,18 +1,77 @@
-class Country{
-    constructor(_name, _pop, _region, _langueges, _coin, _capital){
+import { renderInStart } from "./app.js";
+class Country {
+    constructor(_name, _pop, _region, languages, _coin, _capital, _flag) {
         this.name = _name;
         this.pop = _pop;
         this.region = _region;
-        this.langueges = _langueges;
+        this.languages = languages;
         this.coin = _coin;
         this.capital = _capital;
+        this.flag = _flag;
     }
     render(){
-        const main = document.querySelector("main");
-        const card= document.createElement("div");
-        const information = document.querySelector("dib");
-        const flag = document.createElement("img");
-        
+        // const main = document.querySelector("main");
+        // const container = document.querySelector(".myContainer");
 
+        const card = document.createElement("div");
+        const cards = document.querySelector(".cards");
+        // const cards = document.createElement("div");
+        card.classList = "myCard"
+        cards.classList = "cards"
+        const flag = document.createElement("img");
+        flag.classList = "flag"
+        flag.src = this.flag.png;
+        const name = document.createElement("div");
+        name.textContent = this.name;
+        card.append(flag, name);
+        cards.appendChild(card)
+        card.addEventListener('click', ()=>{
+            console.log("entered");
+            this.renderPop();
+        })
+        
+    }
+    renderPop() {
+        // const main = document.querySelector("main");
+        const arrLan = Object.keys(this.languages);
+        let languagesStr = "";
+        arrLan.forEach((key, index) => {
+            languagesStr += this.languages[key];
+            if (index < arrLan.length - 1) {  
+                languagesStr += ", ";
+            }
+        });
+          
+        const container = document.querySelector(".myContainer");
+        const card = document.createElement("div");
+        const cards = document.querySelector(".cards");
+        // const cards = document.createElement("div");
+        card.classList = "card"
+        cards.classList = "cards"
+        cards.innerHTML = ""
+        const information = document.createElement("div");
+        const flag = document.createElement("img");
+        information.innerHTML = `
+        ${this.name}<br>
+        pop: ${this.pop}<br>
+        region: ${this.region}<br>
+        langueges: ${languagesStr}<br>
+        coin: ${this.coin}<br>
+        capital: ${this.capital}
+        `
+        flag.src = this.flag.png;
+        const back = document.createElement("button");
+        back.textContent = "back";
+        back.addEventListener('click', ()=>{
+            console.log("back entered");
+            renderInStart();
+            
+        })
+
+        container.appendChild(cards);
+        card.append(flag, information, back);
+        cards.appendChild(card)
+        
     }
 }
+export default Country;
